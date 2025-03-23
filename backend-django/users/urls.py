3f13271507180416def2cfde232bc7c871c87a6b
Lovilek <wingeddemon2274@gmail.com>
2025-03-24 00:50:19 +0500

    Reset Password

--- backend-django/users/urls.py
+++ backend-django/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import *
+from users.views import CustomPasswordResetView, CustomPasswordResetConfirmView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -8,5 +9,7 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password-reset/', CustomPasswordResetView.as_view(), name='password-reset'),
+    path('password-reset/confirm/', CustomPasswordResetConfirmView.as_view(), name='password-reset-confirm'),
 
 ]
--- backend-django/users/views.py
+++ backend-django/users/views.py
@@ -1,8 +1,15 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from jwt.utils import force_bytes
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
+
+from backend import settings
 from .models import User
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated,IsAdminUser
@@ -45,3 +52,65 @@ class UserProfileView(generics.RetrieveAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class CustomPasswordResetView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        email = request.data.get('email')
+        login=request.data.get('login')
+        if not email and not login:
+            return Response({"error":"Login и Email обязательны."},status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user=User.objects.get(login__iexact=login)
+        except User.DoesNotExist:
+            return Response({"error":"Пользователь не найден"},status=status.HTTP_404_NOT_FOUND)
+
+        if user.email != email:
+            return Response({"error":"Пользователь не найден"},status=status.HTTP_404_NOT_FOUND)
+
+        uid=urlsafe_base64_encode(force_bytes(str(user.pk)))
+        token=default_token_generator.make_token(user)
+
+        # reset_url=request.build_absolute_uri(f"/reset/{uid}/{token}")
+        reset_url = f"http://localhost:3000/reset-password/{uid}/{token}"
+
+        send_mail(
+            subject="Сброс пароля Narxoz Social",
+            message=f"Ссылка для сброса пароля: {reset_url}",
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email,],
+        )
+        return Response({"message": "Ссылка для сброса отправлена на почту."}, status=status.HTTP_200_OK)
+
+
+class CustomPasswordResetConfirmView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self,request):
+        uid=request.data.get('uid')
+        token=request.data.get('token')
+        new_password=request.data.get('new_password')
+        confirm_new_password=request.data.get('confirm_new_password')
+
+        if new_password != confirm_new_password:
+            return Response({"error":"Пароли не совпадают"},status=status.HTTP_400_BAD_REQUEST)
+
+        if not uid or not token or not new_password:
+            return Response({"error":"Заполните все поля"},status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            uid_decoded=force_str(urlsafe_base64_decode(uid))
+            user=User.objects.get(pk=uid_decoded)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            return Response({"error":"Невалидный uid"},status=status.HTTP_400_BAD_REQUEST)
+
+        if not default_token_generator.check_token(user,token):
+            return Response({"error":"Ссылка недействительна или устарела."},status=status.HTTP_400_BAD_REQUEST)
+
+        user.set_password(new_password)
+        user.save()
+
+        return Response({"message": "Пароль успешно сброшен."},status=status.HTTP_200_OK)
