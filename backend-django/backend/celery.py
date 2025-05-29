fef35d7dc3e86a8437fe6bda9bb6a342db9327fd
Artem <105930858+Lovilek@users.noreply.github.com>
2025-05-30 01:52:10 +0500

    Update celery.py

--- backend-django/backend/celery.py
+++ backend-django/backend/celery.py
@@ -1,44 +1,9 @@
-<<<<<<< HEAD
-# # backend/celery.py
-# import os
-# from datetime import timedelta
-# from celery import Celery
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")  # <-- исправили
-#
-# app = Celery("narxoz_social")
-# app.config_from_object("django.conf:settings", namespace="CELERY")
-# app.autodiscover_tasks()
-#
-# app.conf.update(
-#     task_serializer="json",
-#     accept_content=["json"],
-#     result_serializer="json",
-# )
-#
-# app.conf.beat_schedule = {
-#     "event-reminders-every-5-min": {
-#         "task": "events.tasks.send_event_reminders",
-#         "schedule": timedelta(minutes=5),
-#     },
-# }
-#
-#
-
-=======
->>>>>>> origin/main
-# backend/celery.py
 import os
 from datetime import timedelta
 from celery import Celery
-<<<<<<< HEAD
 from kombu import Queue, Exchange
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-=======
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")  # <-- исправили
->>>>>>> origin/main
 
 app = Celery("narxoz_social")
 app.config_from_object("django.conf:settings", namespace="CELERY")
@@ -48,7 +13,6 @@ app.conf.update(
     task_serializer="json",
     accept_content=["json"],
     result_serializer="json",
-<<<<<<< HEAD
 
 )
 
@@ -71,13 +35,4 @@ app.conf.beat_schedule = {
         "schedule": timedelta(minutes=3),
         "options": {"queue": "celery", "routing_key": "celery"},
     }
-=======
-)
-
-app.conf.beat_schedule = {
-    "event-reminders-every-1-min": {
-        "task": "events.tasks.send_event_reminders",
-        "schedule": timedelta(minutes=1),
-    },
->>>>>>> origin/main
 }
