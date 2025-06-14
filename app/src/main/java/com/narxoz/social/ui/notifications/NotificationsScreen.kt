e9143248a60852b6c3a03ec20bf4416bbda7a7c7
gunstillrattles <xdgm@proton.me>
2025-06-14 09:09:15 +0500

    Fix missing R imports

--- app/src/main/java/com/narxoz/social/ui/notifications/NotificationsScreen.kt
+++ app/src/main/java/com/narxoz/social/ui/notifications/NotificationsScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.narxoz.social.R
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
